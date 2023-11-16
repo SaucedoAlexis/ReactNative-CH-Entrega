c6302a0f4eb11f0b29e946d64332bdc477bd3c2e
Alexis Saucedo <saucedoalexism@gmail.com>
2023-11-15 22:01:48 -0300

    Primer push de entrega para CH

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola, Coder!</Text>
+      <Text>Segundo texto de mi aplicación con ReactNative!</Text>
       <StatusBar style="auto" />
     </View>
   );
